67b3a9aaf94b85c00a5be6b951a248ae9e7b96a3
LycoKodo <164467383+LycoKodo@users.noreply.github.com>
2024-11-18 12:55:26 +1100

    Made multi thread, dk if work, test later

---
+++ [EXP] Multi Thread/src/main.cpp
@@ -0,0 +1,64 @@
+
+#include "main.h"
+#include "lemlib/api.hpp"
+#include "lemlib/chassis/chassis.hpp"
+#include "pros/device.hpp"
+#include "pros/misc.h"
+#include "pros/motors.h"
+#include "pros/rtos.h"
+#include "pros/rtos.hpp"
+#include <sys/wait.h>
+
+#include "robot-config.hpp"
+#include "controls.hpp"
+
+void initialize() {
+    pros::lcd::initialize(); // initialize brain screen
+    colorSort.set_led_pwm(100);
+    // Selecting Alliance Color
+    pros::lcd::register_btn1_cb(setColorSort);
+    pros::lcd::print(3, "[!] ALLIANCE NOT SELECTED, (MID BTN)");
+
+    chassis.calibrate(); // calibrate sensors
+    intake.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+    pros::Task screenTask([]() {
+        while (true) {
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
+            pros::delay(50);
+        }
+    });
+    mogo_mech.set_value(true);
+}
+
+
+void disabled() 
+{
+    // womp womp cant walk
+}
+
+
+void competition_initialize() {
+}
+
+//OPTIMAL Se-TIME for 24 inch (1 tile): 1900
+void autonomous() 
+{
+    chassis.setPose(0,0,0);
+    intake.move(-127);
+    Intake_SortedMove(-127, 2000, 0.5, true);
+}
+
+
+void opcontrol() 
+{
+    chassis.setBrakeMode(pros::E_MOTOR_BRAKE_COAST);
+    intake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    
+    pros::Task intakeTask(intake_control);
+    pros::Task mogoTask(mogo_control);
+    pros::Task driveTask(drivetrain_control);
+}
+
